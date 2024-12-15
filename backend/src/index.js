3f26432b36a88221019f74298ae7ae0749fa3e31
sharafath ali <sharafathaliofficial@gmail.com>
2024-12-15 21:58:08 +0530

    db setup completed and added a new user model

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,16 @@
 import express from 'express'
 import authRoutes from './routes/authRoutes.js'
+import { connectDB } from './lib/db.js'
 
+import { configDotenv } from "dotenv";
+configDotenv()
+
+const Port = process.env.PORT
 const app = express()
-const port = 3000
 
 app.get('/api/auth', authRoutes)
 
-app.listen(port, () => {
-  console.log(`app listening on port ${port}`)
-})
+app.listen(Port, () => {
+  console.log(`Server is running on 'http://localhost:${Port}'`);
+  connectDB()
+});
---
+++ backend/src/models/user.model.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  fullName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6,
+  },
+  profilePic: {
+    type: String,
+    default: "",
+  },
+  isAdmin: {
+    type: Boolean,
+    required: true,
+    default: false,
+  },
+}, { timestamps: true });
+
+export default mongoose.model("User", userSchema);
