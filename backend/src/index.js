38e454d293a3010924241d5478e965387809bd0b
sharafath ali <sharafathaliofficial@gmail.com>
2024-12-27 01:59:26 +0530

    added changes for deploying the app

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,11 +6,14 @@ import { configDotenv } from "dotenv";
 import cookieParser from "cookie-parser";
 import messageRoutes from './routes/message.route.js'
 import cors from 'cors'
-import { app , server , io } from './lib/socket.js'
+import { app, server, io } from './lib/socket.js'
+
+import path from "path";
 
 configDotenv()
 
 const Port = process.env.PORT
+const __dirname = path.resolve();
 app.use(cors({
   origin: 'http://localhost:5173',
   credentials: true
@@ -21,6 +24,14 @@ app.use(express.json({ limit: '50mb' }));
 
 app.use('/api/auth', authRoutes)
 app.use('/api/message', messageRoutes)
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 server.listen(Port, () => {
   console.log(`Server is running on 'http://localhost:${Port}'`);
